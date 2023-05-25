f01986abfea2c9532452047b1e3939cf16e958bb
Sonja Parsell <sonjaparsell09@gmail.com>
2023-05-25 10:10:28 -0400

    adds header and images

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -26,6 +26,7 @@ function BookShow({ book, onDelete, onEdit }) {
     return (
         <div className='book-show'>
           <div>{content}</div>
+            <img src={`https://picsum.photos/seed/${book.id}/300/200`} alt="books"/>
           <div className='actions'>
             <button className='edit' onClick={handleEditClick}></button>
             <button className="delete" onClick={handleDeleteClick}></button>

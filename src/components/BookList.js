ec8b9af07c133a6ea79f3c9f9510a22511252ea1
Sonja Parsell <sonjaparsell09@gmail.com>
2023-06-05 09:33:10 -0400

    adds context

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,20 @@
+import { useContext } from 'react';
+import BooksContext from '../context/books';
 import BookShow from './BookShow';
 
+
 function BookList({ books, onDelete, onEdit }) {
+    const { count, incrementCount } = useContext(BooksContext);
+
     const renderBooks = books.map((book) => {
         return <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit}/>;
     })
     
     return (
         <div className='book-list'>
-              {renderBooks}
+               {count}
+              <button onClick={incrementCount}>Click</button>
+           
         </div>
     );
 };
